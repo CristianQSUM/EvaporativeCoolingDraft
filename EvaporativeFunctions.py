8ffc8ea4f0fadf523e0ed1be9a9e826b86ed4b20
CristianQSUM <cristian.ramirez@unb.ca>
2025-05-01 14:46:25 -0300

    Update EvaporativeFunctions.py
    Edited class CrossedDipoleTrap, still need to edit NTdependant functions

--- EvaporativeFunctions.py
+++ EvaporativeFunctions.py
@@ -13,6 +13,7 @@ class CrossedDipoleTrap:
     a_0 = constants.physical_constants['Bohr radius'][0]
     a = 98*a_0
 
+
     def __init__(self, timearray, power1, power2, waist1, waist2, fU1 = 1.0, fU2 = 1.0, fw1 = 1.0, fw2 = 1.0,
                  wavelength = 1064e-9, alpha_natural = alpha_natural, mass = m, k_Boltz = Boltzmann, scattering_length = a):
         self.wavelength = wavelength
@@ -25,57 +26,60 @@ class CrossedDipoleTrap:
         self.power2 = power2
         self.waist1 = waist1
         self.waist2 = waist2
-        self.beam1depth = single_beam_depth(power1, waist1, fU1)
-        self.beam2depth = single_beam_depth(power2, waist2, fU2)
-        self.trapdepth = trapdepth(beam1depth, beam2depth)
-        self.omegax = omega_x(beam_frequency_squared_in_mod_direction(power1, waist1, fU1), beam_frequency_squared_in_prop_direction(power2, waist2, fU2))
-        self.omegay = omega_y(beam_frequency_squared_in_vert(power1, waist1, fU1), beam_frequency_squared_in_vert(power2, waist2, fU2))
-        self.omegaz = omega_z(beam_frequency_squared_in_prop_direction(power1, waist1, fU1), beam_frequency_squared_in_mod_direction(power2, waist2, fU2))
-        self.omegabar = geometric_mean_freq(omegax, omegay, omegaz)
-        self.trapfrequencymodulation = omega_bar_dot_over_omega_bar(omegax, omegay, omegaz)
+        self.beam1depth = self.single_beam_depth(power1, waist1, fU1)
+        self.beam2depth = self.single_beam_depth(power2, waist2, fU2)
+        self.trapdepth = self.trap_depth(self.beam1depth, self.beam2depth)
+        self.omegax = self.omega_x(self.beam_frequency_squared_in_mod_direction(power1, waist1, fU1), self.beam_frequency_squared_in_prop_direction(power2, waist2, fU2))
+        self.omegay = self.omega_y(self.beam_frequency_squared_in_vert(power1, waist1, fU1), self.beam_frequency_squared_in_vert(power2, waist2, fU2))
+        self.omegaz = self.omega_z(self.beam_frequency_squared_in_prop_direction(power1, waist1, fU1), self.beam_frequency_squared_in_mod_direction(power2, waist2, fU2))
+        self.omegabar = self.geometric_mean_freq(self.omegax, self.omegay, self.omegaz)
+        self.trapfrequencymodulation = self.omega_bar_dot_over_omega_bar(self.omegax, self.omegay, self.omegaz)
     
-    def Rayleigh(waist, l=1064e-9):
+    def single_beam_depth(self, powerarray, waist, f_U_array = 1.0, atomicpolarizability=alpha_natural):
+        from scipy.constants import pi
+        unmodulateddepth = (2*atomicpolarizability/pi)*(powerarray/waist**2)
+        epsilon_t = unmodulateddepth*f_U_array
+        return(epsilon_t)
+    
+    def Rayleigh(self, waist, l=1064e-9):
+        from scipy.constants import pi
         z_r = pi*waist**2/l
         return(z_r)
 
-    def single_beam_depth(powerarray, waist, f_U_array = 1.0, atomicpolarizability=alpha_natural):
-      unmodulateddepth = (2*atomicpolatizability/pi)*(powerarray/waist**2)
-      epsilon_t = unmodulateddepth*f_U_array
-      return(epsilon_t)
-
-    def trap_depth(beam1depth, beam2depth):
+    def trap_depth(self, beam1depth, beam2depth):
       U_0 = beam1depth + beam2depth
       return(U_0)
 
-    def beam_frequency_squared_in_prop_direction(powerarray, waist, f_U_array = 1.0, atomicpolarizability = alpha_natural, mass = m):
-        depth = single_beam_depth(powerarray, waist, f_U_array, atomicpolarizability)
-        z_R = Rayleigh(waist)
+    def beam_frequency_squared_in_prop_direction(self, powerarray, waist, f_U_array = 1.0, atomicpolarizability = alpha_natural, mass = m):
+        depth = self.single_beam_depth(powerarray, waist, f_U_array, atomicpolarizability)
+        z_R = self.Rayleigh(waist)
         omega_squared = 2*depth/(mass*z_R**2)
         return(omega_squared)
 
-    def beam_frequency_squared_in_mod_direction(powerarray, waist, f_w_array = 1.0, atomicpolarizability = alpha_natural, mass = m):
+    def beam_frequency_squared_in_mod_direction(self, powerarray, waist, f_w_array = 1.0, atomicpolarizability = alpha_natural, mass = m):
+        from scipy.constants import pi
         omega_squared = (8*atomicpolarizability*powerarray*f_w_array)/(pi*mass*waist**4)
         return(omega_squared)
 
-    def beam_frequency_squared_in_vert(powerarray, waist, f_U_array = 1.0, atomicpolarizability = alpha_natural, mass = m):
-        depth = single_beam_depth(powerarray, waist, f_U_array, atomicpolarizability)
+    def beam_frequency_squared_in_vert(self, powerarray, waist, f_U_array = 1.0, atomicpolarizability = alpha_natural, mass = m):
+        depth = self.single_beam_depth(powerarray, waist, f_U_array, atomicpolarizability)
         omega_squared = 4*depth/(mass*waist**2)
         return(omega_squared)
 
-    def omega_x(beam1modomegasquared, beam2propomegasquared):
+    def omega_x(self, beam1modomegasquared, beam2propomegasquared):
         return(np.sqrt(beam1modomegasquared + beam2propomegasquared))
 
-    def omega_y(beam1vertsquared, beam2vertsquared):
+    def omega_y(self, beam1vertsquared, beam2vertsquared):
         return(np.sqrt(beam1vertsquared+beam2vertsquared))
 
-    def omega_z(beam1propomegasquared, beam2modomegasquared):
+    def omega_z(self, beam1propomegasquared, beam2modomegasquared):
         return(np.sqrt(beam1propomegasquared + beam2modomegasquared))
 
-    def geometric_mean_freq(omegax, omegay, omegaz):
+    def geometric_mean_freq(self, omegax, omegay, omegaz):
         return(np.cbrt(omegax*omegay*omegaz))
 
-    def omega_bar_dot_over_omega_bar(omegax, omegay, omegaz):
-        omegabar = geometric_mean_freq(omegax, omegay, omegaz)
+    def omega_bar_dot_over_omega_bar(self, omegax, omegay, omegaz):
+        omegabar = self.geometric_mean_freq(omegax, omegay, omegaz)
         return(np.gradient(omegabar)/omegabar)
 
 class NTdependentfunctions:
