7cd0adc19c740865a500f24833246e7a2655095b
CristianQSUM <cristian.ramirez@unb.ca>
2025-05-20 18:52:56 -0300

    Update SimpleSpatialModulation.py

--- SimpleSpatialModulation.py
+++ SimpleSpatialModulation.py
@@ -14,21 +14,28 @@ def mod_position(timearray, h, f=8e7):
 
 def f_U(w, h, modfreq=8e7, nSamples = 100): #increase nSamples for better accuracy at the cost of computation time
     from scipy.integrate import simpson as simps
-    t_array = np.linspace(0, 1/modfreq, nSamples)
-    x_t = mod_position(t_array, h, modfreq)
-    integrand = np.exp(-2 * x_t**2/w**2)
-    f_u = modfreq*simps(integrand, t_array)
-    return f_u
+    f_u_array = []
+    for hvalue in h: #will vectorize later
+        t_array = np.linspace(0, 1/modfreq, nSamples)
+        idx += 1 
+        x_t = mod_position(t_array, hvalue, modfreq)
+        integrand = np.exp(-2 * x_t**2/w**2)
+        f_u = modfreq*simps(integrand, t_array)
+        f_u_array.append(f_u)
+    return f_u_array
 
 def f_omega(w, h, f=8e7, nSamples=100): #w is waist, h is modulation amplitude
     from scipy.integrate import simpson as simps
     t_array = np.linspace(0, 1/f, nSamples)
-    x_t = mod_position(t_array, harray, f)
-    integrand = ((16 * x_t**2 - 4 * w**2)/w**4) * np.exp(-2 * x_t**2/w**2)
-    average_curvature = f * simps(integrand, t_array)
-    curvature_static = (-4/w**2)
-    f_w = average_curvature/curvature_static
-    return f_w
+    f_w_array = []
+    for hvalue in h:
+        x_t = mod_position(t_array, hvalue, f)
+        integrand = ((16 * x_t**2 - 4 * w**2)/w**4) * np.exp(-2 * x_t**2/w**2)
+        average_curvature = f * simps(integrand, t_array)
+        curvature_static = (-4/w**2)
+        f_w = average_curvature/curvature_static
+        f_w_array.append(f_w)
+    return f_w_array
 
 
 
